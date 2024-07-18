9316aa553cb295a518df85aab0add74e0a29ef34
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-07-18 04:58:13 +0300

    check

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-"""
-"""
-
+'''Task 12's module.
+'''
 from pymongo import MongoClient
 
-def print_nginx_request_logs(nginux_collection):
-    """"""
-    print('{} logs'.format(nginux_collection.count_documents({})))
+
+def print_nginx_request_logs(nginx_collection):
+    '''Prints stats about Nginx request logs.
+    '''
+    print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = len(list(nginux_collection.find({'method': method})))
-        print(f'\tmethod {method}: {req_count}')
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
     status_checks_count = len(list(
-        nginux_collection.find({'method': 'GET', 'path': '/status'})
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
     print('{} status check'.format(status_checks_count))
 
@@ -27,5 +28,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-
-    
