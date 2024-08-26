04babf046c074bd80a9b7eea1daec0fd5243ba41
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-26 22:33:01 +0300

    check 0. Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" exercise module
+"""
+import redis
+import uuid
+from typing import Any
+
+
+class Cache():
+    """ caching system
+    """
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        """ used to store data in the cache.
+        """
+        key = str(uuid.uuid4())
+
+        if isinstance(data, (int, float)):
+            value = str(data)
+        elif isinstance(data, bytes):
+            value = data.decode('utf-8')
+        elif isinstance(data, str):
+            value = data
+        else:
+            raise TypeError("Unsupported data type")
+
+        self._redis.set(key, value)
+        return key
