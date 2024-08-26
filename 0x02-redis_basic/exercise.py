bf372c703eae395c8f4145c48666fccc8416487d
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-27 01:07:26 +0300

    check

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,20 +21,19 @@ def count_calls(method: Callable) -> Callable:
 
 
 def call_history(method: Callable) -> Callable:
-    """ store the history of inputs and outputs.
-    """
+    '''Tracks the call details of a method in a Cache class.
+    '''
     @wraps(method)
     def invoker(self, *args, **kwargs) -> Any:
-        """ Invokes the given method after caching it's input and output.
-        """
-        input_key = method.__qualname__ + ":input"
-        output_key = method.__qualname__ + ":output"
+        '''Returns the method's output after storing its inputs and output.
+        '''
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
         if isinstance(self._redis, redis.Redis):
-            self._redis.rpush(input_key, str(args))
-
-        output = self.method(*args, **kwargs)
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
         if isinstance(self._redis, redis.Redis):
-            self._redis.rpush(output_key, str(output))
+            self._redis.rpush(out_key, output)
         return output
     return invoker
 
